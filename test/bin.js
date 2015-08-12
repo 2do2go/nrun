f55645198932c00f8965205cbac8f721b71897f7
oleg <oleg.korobenko@gmail.com>
2015-08-12 16:15:03 +0300

    add tests

---
+++ test/bin.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var expect = require('expect.js'),
+	Steppy = require('twostep').Steppy,
+	childProcess = require('child_process');
+
+describe('Binary', function() {
+
+	var binPath = 'bin/nrun';
+
+	var exec = function() {
+		var callback = arguments[arguments.length - 1];
+		arguments[arguments.length - 1] = function(err, stdout, stderr) {
+			callback(err, {stdout: stdout, stderr: stderr});
+		};
+		childProcess.exec.apply(childProcess.exec, arguments);
+	};
+
+	it('call without arguments should return list of scripts', function(done) {
+		Steppy(
+			function() {
+				exec(binPath, this.slot());
+			},
+			function(err, binData) {
+				expect(binData.stderr).not.ok();
+				expect(binData.stdout).eql(
+					[
+						'Scripts for "nrun":',
+						'  test',
+						'    mocha test/ --bail --reporter spec',
+						'  testls',
+						'    ls'
+					].join('\n') + '\n'
+				);
+				this.pass(null);
+			},
+			done
+		);
+	});
+
+	it('call with single arg calls target script', function(done) {
+		Steppy(
+			function() {
+				exec(binPath + ' testls', this.slot());
+				exec('ls', this.slot());
+			},
+			function(err, binData, lsData) {
+				expect(binData.stderr).not.ok();
+				expect(binData.stdout).eql(
+					[
+						'> nrun testls',
+						'> ls ',
+						lsData.stdout
+					].join('\n')
+				);
+				this.pass(null);
+			},
+			done
+		);
+	});
+
+	it('call with more args calls target script args', function(done) {
+		Steppy(
+			function() {
+				exec(binPath + ' testls -l -h', this.slot());
+				exec('ls -l -h', this.slot());
+			},
+			function(err, binData, lsData) {
+				expect(binData.stderr).not.ok();
+				expect(binData.stdout).eql(
+					[
+						'> nrun testls',
+						'> ls "-l" "-h"',
+						lsData.stdout
+					].join('\n')
+				);
+				this.pass(null);
+			},
+			done
+		);
+	});
+
+	it('call with unexisted script throw error to stderr', function(done) {
+		exec(binPath + ' unexisted', function(err, binData) {
+			expect(err).ok();
+			expect(err.message).match(/Unknown script: unexisted/);
+			done();
+		});
+	});
+});
