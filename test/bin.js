478209b57ed11c74cf3dae706d14ec47f3ac5644
oleg <oleg.korobenko@gmail.com>
2016-01-27 18:12:10 +0300

    add binaries to path

--- test/bin.js
+++ test/bin.js
@@ -4,7 +4,8 @@ var expect = require('expect.js'),
 	Steppy = require('twostep').Steppy,
 	childProcess = require('child_process'),
 	fs = require('fs'),
-	path = require('path');
+	path = require('path'),
+	os = require('os');
 
 var completionShContent = fs.readFileSync(
 	path.join(__dirname, '../bin/completion.sh'), 'utf8'
@@ -14,12 +15,7 @@ var isWin = process.platform.substring(0, 3) === 'win';
 
 describe('Binary', function() {
 
-	var binPath;
-	if (!isWin) {
-		binPath = 'bin/nrun';
-	} else {
-		binPath = 'node bin/nrun';
-	}
+	var binPath = isWin ? 'node bin/nrun' : 'bin/nrun';
 
 	var exec = function() {
 		var callback = arguments[arguments.length - 1];
@@ -41,8 +37,12 @@ describe('Binary', function() {
 						'Scripts for "nrun":',
 						'  test',
 						'    mocha test/ --bail --reporter spec',
-						'  testls',
-						'    ls'
+						'  testLs',
+						'    ls',
+						'  testShowPath',
+						'    echo $PATH',
+						'  testShowPathWin',
+						'    echo %PATH%'
 					].join('\n') + '\n'
 				);
 				this.pass(null);
@@ -54,14 +54,14 @@ describe('Binary', function() {
 	it('call with single arg calls target script', function(done) {
 		Steppy(
 			function() {
-				exec(binPath + ' testls', this.slot());
+				exec(binPath + ' testLs', this.slot());
 				exec('ls', this.slot());
 			},
 			function(err, binData, lsData) {
 				expect(binData.stderr).not.ok();
 				expect(binData.stdout).eql(
 					[
-						'> nrun testls',
+						'> nrun testLs',
 						'> ls ',
 						lsData.stdout
 					].join('\n')
@@ -72,17 +72,44 @@ describe('Binary', function() {
 		);
 	});
 
+	it('should extend path with node_modules/.bin', function(done) {
+		var script = isWin ? 'testShowPathWin' : 'testShowPath',
+			cmd = isWin ? 'echo %PATH%' : 'echo $PATH';
+
+		Steppy(
+			function() {
+				exec(binPath + ' ' + script, this.slot());
+				exec(cmd, this.slot());
+			},
+			function(err, binData, cmdData) {
+				expect(binData.stderr).not.ok();
+				expect(binData.stdout).eql(
+					[
+						'> nrun ' + script,
+						'> ' + cmd + ' ',
+						cmdData.stdout.replace(
+							new RegExp(os.EOL + '$'),
+							path.delimiter + 'node_modules/.bin' + os.EOL
+						)
+					].join(os.EOL)
+				);
+				this.pass(null);
+			},
+			done
+		);
+	});
+
 	it('call with more args calls target script args', function(done) {
 		Steppy(
 			function() {
-				exec(binPath + ' testls -l -h', this.slot());
+				exec(binPath + ' testLs -l -h', this.slot());
 				exec('ls -l -h', this.slot());
 			},
 			function(err, binData, lsData) {
 				expect(binData.stderr).not.ok();
 				expect(binData.stdout).eql(
 					[
-						'> nrun testls',
+						'> nrun testLs',
 						'> ls "-l" "-h"',
 						lsData.stdout
 					].join('\n')
