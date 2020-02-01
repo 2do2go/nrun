5cdfeecf51d7753e1d45f9ae558e3f9121841d83
Mikhail Vyrodov <me.vyrodov@ya.ru>
2020-02-01 19:04:25 +0300

    refactor source code

--- bin/nrun.js
+++ bin/nrun.js
@@ -4,67 +4,79 @@ const fs = require('fs');
 const path = require('path');
 const { addArguments } = require('./helpers');
 const { writeStdout, writeStderr, spawnCommand } = require('./utils');
-const { name, scripts } = require(path.join(process.cwd(), 'package.json'));
+
+const {
+  name: packageName,
+  scripts: packageScripts,
+} = require(path.join(process.cwd(), 'package.json'));
+
+
+function getScriptsToRun(scriptName, scriptArgs) {
+  return ['pre', '', 'post']
+    .filter((prefix) => packageScripts[prefix + scriptName])
+    .map((prefix) => {
+      const name = prefix + scriptName;
+      const args = prefix ? [] : scriptArgs;
+
+      return {
+        name,
+        command: addArguments(packageScripts[name], args),
+      };
+    });
+}
 
 async function exit(code) {
-	await Promise.all([writeStdout(''), writeStderr('')]);
-	process.exit(code);
+  await Promise.all([writeStdout(''), writeStderr('')]);
+  process.exit(code);
 }
 
+/* eslint-disable no-console */
 async function runScripts(scripts) {
-	for (const [index, { name, command }] of Object.entries(scripts)) {
-		console.log('> nrun %s', name);
-		console.log('> %s', command);
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [index, { name, command }] of Object.entries(scripts)) {
+    console.log('> nrun %s', name);
+    console.log('> %s', command);
 
-		await spawnCommand(command);
+    await spawnCommand(command);
 
-		if (index < scripts.length - 1) {
-			console.log('');
-		}
-	}
+    if (index < scripts.length - 1) {
+      console.log('');
+    }
+  }
 }
 
-const [, , scriptName, ...scriptArgs] = process.argv;
+async function validateArgs([, , scriptName, ...scriptArgs]) {
+  if (!scriptName) {
+    console.log(`Scripts for "${packageName}":`);
+    Object.entries(packageScripts).forEach(([name, script]) => {
+      console.log(`  ${name}`);
+      console.log(`    ${script}`);
+    });
 
-if (!scriptName) {
-	console.log(`Scripts for "${name}":`);
+    return exit(0);
+  }
 
-	Object.entries(scripts).forEach(([name, script]) => {
-		console.log(`  ${name}`);
-		console.log(`    ${script}`);
-	});
+  if (scriptName === '--completion') {
+    console.log(
+      fs.readFileSync(
+        path.join(__dirname, 'completion.sh'),
+        'utf8',
+      ),
+    );
 
-	return exit(0);
+    return exit(0);
+  }
+
+  if (!packageScripts[scriptName]) {
+    console.error(`Unknown script "${scriptName}" for package "${packageName}"`);
+    return exit(1);
+  }
+
+  return { scriptName, scriptArgs };
 }
+/* eslint-enable */
 
-if (scriptName === '--completion') {
-	console.log(
-		fs.readFileSync(
-			path.join(__dirname, 'completion.sh'),
-			'utf8'
-		)
-	);
-
-	return exit(0);
-}
-
-if (!scripts[scriptName]) {
-	console.error(`Unknown script "${scriptName}" for package "${name}"`);
-	return exit(1);
-}
-
-const scriptsToRun = ['pre', '', 'post']
-	.filter((prefix) => scripts[prefix + scriptName])
-	.map((prefix) => {
-		const name = prefix + scriptName;
-		const args = prefix ? [] : scriptArgs;
-
-		return {
-			name,
-			command: addArguments(scripts[name], args)
-		};
-	});
-
-runScripts(scriptsToRun)
-	.then(exit)
-	.catch(exit);
+validateArgs(process.argv)
+  .then(({ scriptName, scriptArgs }) => runScripts(getScriptsToRun(scriptName, scriptArgs)))
+  .then(exit)
+  .catch(exit);
